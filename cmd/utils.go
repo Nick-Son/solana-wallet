74a031befa04a6a568473839b91929f459ce5390
Nick Sonner <nicksonner@gmail.com>
2022-01-05 13:13:43 +1100

    Added CreateNewWallet function in utils.go, updated Run command in createWallet.go to execute CreateNewWallet

--- cmd/utils.go
+++ cmd/utils.go
@@ -1,13 +1,29 @@
 package cmd
 
 import (
+	"io/ioutil"
+	"log"
+
 	"github.com/portto/solana-go-sdk/client"
 	"github.com/portto/solana-go-sdk/types"
 )
 
 type Wallet struct {
-	// Holds the wallet object
-	account types.Account
-	// Holds the RPC client object used to connect to Solana networks
-	c *client.Client
+	account types.Account  // Holds the wallet object
+	c       *client.Client // Holds the RPC client object used to connect to Solana networks
+}
+
+func CreateNewWallet(RPCEndpoint string) Wallet {
+	newAccount := types.NewAccount()      // create a new wallet using types.NewAccount()
+	data := []byte(newAccount.PrivateKey) // Convert the private key to byte array
+
+	err := ioutil.WriteFile("data", data, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return Wallet{
+		newAccount,
+		client.NewClient(RPCEndpoint),
+	}
 }
